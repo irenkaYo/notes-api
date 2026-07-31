0b21b0c6af2baf304dd3a12f1e659498f99dc9a1
irenaYo <irenatugushi997@gmail.com>
2026-07-31 13:50:20 +0300

    update MeResponse

--- NotesApi/Application/DTOs/Responses/MeResponse.cs
+++ NotesApi/Application/DTOs/Responses/MeResponse.cs
@@ -2,10 +2,10 @@ namespace Application.DTOs.Responses;
 
 public class MeResponse
 {
-    public string UserId { get; set; }
-    public string Username { get; set; }
+    public string? UserId { get; set; }
+    public string? Username { get; set; }
 
-    public MeResponse(string userId, string username)
+    public MeResponse(string? userId, string? username)
     {
         UserId = userId;
         Username = username;
