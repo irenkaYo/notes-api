0bc31d1e08c2f042a38270e9cdf458a382012c61
irenaYo <irenatugushi997@gmail.com>
2026-08-02 14:01:12 +0300

    allow nullable text, public setters, and use UtcNow

--- NotesApi/Domain/Models/Note.cs
+++ NotesApi/Domain/Models/Note.cs
@@ -3,18 +3,18 @@ namespace Domain.Models;
 public class Note
 {
     public Guid Id { get; private set; }
-    public string Title { get; private set; }
-    public string Text { get; private set; }
+    public string Title { get; set; }
+    public string? Text { get;  set; }
     public DateTime CreatedAt { get; private set; }
     public Guid UserId { get; private set; }
     public User User { get; private set; } = null!;
     
-    public Note(string title, string text, Guid userId)
+    public Note(string title, string? text, Guid userId)
     {
         Id = Guid.NewGuid();
         Title = title;
         Text = text;
-        CreatedAt = DateTime.Now;
+        CreatedAt = DateTime.UtcNow;
         UserId = userId;
     }
 }
