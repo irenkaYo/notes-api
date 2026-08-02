eae270c8ac98f0fc259373d1c51a725b65f5e312
irenaYo <irenatugushi997@gmail.com>
2026-08-02 14:03:12 +0300

    add Id to NoteResponse

--- NotesApi/Application/DTOs/Responses/NoteResponse.cs
+++ NotesApi/Application/DTOs/Responses/NoteResponse.cs
@@ -2,12 +2,14 @@ namespace Application.DTOs.Responses;
 
 public class NoteResponse
 {
+    public Guid Id { get; set; }
     public string Title { get; set; }
     public string Text { get; set; }
     public DateTime CreatedAt { get; set; }
 
-    public NoteResponse(string title, string text, DateTime createdAt)
+    public NoteResponse(Guid id, string title, string text, DateTime createdAt)
     {
+        Id = id;
         Title = title;
         Text = text;
         CreatedAt = createdAt;
