c4665720894412831541beb21d71d79854ab39e2
irenaYo <irenatugushi997@gmail.com>
2026-08-03 17:38:13 +0300

    add method to generate refresh token

--- NotesApi/Application/Interfaces/Authentication/IJwtProvider.cs
+++ NotesApi/Application/Interfaces/Authentication/IJwtProvider.cs
@@ -5,4 +5,5 @@ namespace Application.Interfaces.Authentication;
 public interface IJwtProvider
 {
     public string GenerateToken(User user);
+    public Task<string> GenerateRefreshToken(string token, User user, string? existingRefreshToken);
 }
--- NotesApi/Infrastructure/Authentication/JwtProvider.cs
+++ NotesApi/Infrastructure/Authentication/JwtProvider.cs
@@ -2,6 +2,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using Application.Interfaces.Authentication;
+using Application.Interfaces.Repositories;
 using Domain.Models;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
@@ -11,10 +12,12 @@ namespace Infrastructure.Authentication;
 public class JwtProvider : IJwtProvider
 {
     private readonly JwtOptions _jwtOptions;
+    private readonly IRefreshTokenRepository _refreshTokenRepository;
     
-    public JwtProvider(IOptions<JwtOptions> options)
+    public JwtProvider(IOptions<JwtOptions> options, IRefreshTokenRepository refreshTokenRepository)
     {
         _jwtOptions = options.Value;
+        _refreshTokenRepository = refreshTokenRepository;
     }
     public string GenerateToken(User user)
     {
@@ -30,9 +33,34 @@ public class JwtProvider : IJwtProvider
         var token = new JwtSecurityToken(
             signingCredentials: signingCredentials,
             claims: claims,
-            expires: DateTime.UtcNow.AddHours(_jwtOptions.ExpiresHours));
+            expires: DateTime.UtcNow.AddMinutes(_jwtOptions.ExpiresHours));
         
         var tokenHandler = new JwtSecurityTokenHandler();
         return tokenHandler.WriteToken(token);
+    }
+    
+    public async Task<string> GenerateRefreshToken(string token, User user, string? existingRefreshToken)
+    {
+        var refreshToken = new RefreshToken
+        {
+            Token = Guid.NewGuid().ToString(),
+            UserId = user.Id,
+            ExpiryDate = DateTime.UtcNow.AddDays(7),
+            CreatedAt = DateTime.UtcNow
+        };
+
+        if (!string.IsNullOrEmpty(existingRefreshToken))
+        {
+            var existingToken = await _refreshTokenRepository.GetByToken(existingRefreshToken);
+
+            if (existingToken != null)
+            {
+                await _refreshTokenRepository.DeleteToken(existingToken);
+            }
+        }
+
+        await _refreshTokenRepository.AddToken(refreshToken);
+
+        return refreshToken.Token;
     }
 }
