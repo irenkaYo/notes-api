068da7383b44a7be7828870ece98ea99a8c606f0
irenaYo <irenatugushi997@gmail.com>
2026-08-03 18:50:28 +0300

    update JwtOptions

--- NotesApi/Infrastructure/Authentication/JwtOptions.cs
+++ NotesApi/Infrastructure/Authentication/JwtOptions.cs
@@ -4,4 +4,6 @@ public class JwtOptions
 {
     public string SecretKey { get; set; }
     public int ExpiresHours { get; set; }
+    public string Issuer { get; set; }
+    public string Audience { get; set; }
 }
